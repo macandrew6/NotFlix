22256b301ebbeba15fc94ab8e036eb008f2d02d7
macandrew6 <mr.betterandrewmac@gmail.com>
2019-04-30 20:40:10 -0700

    sets up all neccessary session actions

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -2,6 +2,7 @@ import * as SessionUtil from 'session';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveCurrentUser = user => ({
   type: RECEIVE_CURRENT_USER,
@@ -12,14 +13,25 @@ const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER
 });
 
+const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
 export const signUp = userForm => dispatch => (
-  SessionUtil.postUser(userForm)
-    .then(user => dispatch(receiveCurrentUser(user)))
+  SessionUtil.postUser(userForm).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
 );
 
 export const login = userForm => dispatch => (
-  SessionUtil.postSession(userForm)
-    .then(user => dispatch(receiveCurrentUser(user)))
+  SessionUtil.postSession(userForm).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
 );
 
 export const logout = () => dispatch => (
