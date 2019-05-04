45b3e3d9391d1af2dac419188dcd9edecb1245f6
macandrew6 <mr.betterandrewmac@gmail.com>
2019-05-04 14:51:41 -0700

    uses React-Player to play media on mouseEnter

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
+Jbuilder.key_format camelize: :lower
 # Initialize the Rails application.
 Rails.application.initialize!
--- frontend/components/browse/movie.jsx
+++ frontend/components/browse/movie.jsx
@@ -6,48 +6,56 @@ class Movie extends React.Component {
     super(props);
     this.state = {
       playing: false,
-      loop: false
+      loop: false,
+      controls: false,
+      light: this.props.movie.imageUrl, // this isn't working properly
+      volume: 0
     };
-
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseOut = this.handleMouseOut.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
-  handleMouseEnter(e) {
-    setTimeout(()=>{
-      this.setState({
-        playing: !this.state.playing,
-        loop: !this.state.loop
-      });
-    }, 300);
+  handleMouseEnter() {
+    this.setState({
+      playing: !this.state.playing,
+      loop: !this.state.loop,
+      light: false, // isn't working properly
+      volume: 1
+    });
   }
 
-  handleMouseOut() {
+  handleMouseLeave() {
     this.setState({
       playing: false,
-      loop: false
+      loop: false,
+      light: this.props.movie.imageUrl, // isn't working properly
+      volume: 0
     });
   }
 
   render() {
     const { movie } = this.props;
+    const { volume, light, loop, playing, controls} = this.state;
+
     return (
       <li 
         className="temp-single-movie-container"
         onMouseEnter={this.handleMouseEnter}
-        onMouseOut={this.handleMouseOut}
+        onMouseLeave={this.handleMouseLeave}
         >
         <ReactPlayer
+          muted={true}
           width="100%"
           height="100%"
-          loop={this.state.loop}
-          controls
-          playing={this.state.playing}
-          url={movie.movie_url}
+          volume={null}
+          playing={playing}
+          url={movie.movieUrl}
         />
       </li>
     );
   }
 }
+
+// onstart render a button that controls volume and a place holder button for adding movie to list
 
 export default Movie;
