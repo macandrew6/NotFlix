cf488d334240d0c970051c7f54547a9d11fae79b
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-19 20:38:23 -0700

    removes user movies reducer comments

--- frontend/actions/my_list.js
+++ frontend/actions/my_list.js
@@ -1,17 +1,11 @@
 import * as UserMoviesUtil from '../util/user_movies';
 
 export const RECEIVE_USER_MOVIES = 'RECEIVE_USER_MOVIES';
-// export const RECEIVE_USER_MOVIE = 'RECEIVE_USER_MOVIE';
 
 const receiveUserMovies = (userMovies) => ({
   type: RECEIVE_USER_MOVIES,
   userMovies
 }); 
-
-// const receiveUserMovie = (userMovie) => ({
-//   type: RECEIVE_USER_MOVIE,
-//   userMovie
-// });
 
 export const fetchUserMovies = () => dispatch => (
   UserMoviesUtil.fetchUserMovies()
