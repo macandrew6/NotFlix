92485ff1efa2015bed99b5c9295dd7b56a7a0de8
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-28 17:50:03 -0700

    debuggs moviethumbnail container

--- frontend/components/browse/my_lists_components/my_list_container.js
+++ frontend/components/browse/my_lists_components/my_list_container.js
@@ -3,12 +3,15 @@ import { withRouter } from 'react-router-dom';
 import { fetchUserMovies } from '../../../actions/my_list_action.js';
 import MyList from './my_list';
 
-const mapStateToProps = ({ entities: { userMovies, movies, users }, session}) => ({
-  user: users[session.id],
-  movies: Object.values(movies).filter(movie => {
-    return userMovies[movie.id];
-  })
-});
+const mapStateToProps = ({ entities: { userMovies, movies, users }, session}) => {
+  console.log(userMovies);
+  return {
+    user: users[session.id],
+    movies: Object.values(movies).filter(movie => {
+      return userMovies[movie.id];
+    })
+  };
+};
 
 
 const mapDispatchToProps = dispatch => ({
