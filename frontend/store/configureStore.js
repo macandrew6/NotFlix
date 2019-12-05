3443e7b9da1d35a389f54b1a73b6909f333e362d
macandrew6 <mr.betterandrewmac@gmail.com>
2019-12-05 10:42:25 -0800

    removes logger from app

--- frontend/store/configureStore.js
+++ frontend/store/configureStore.js
@@ -1,17 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import rootReducer from '../reducers/root';
 
 const configureStore = (preloadedState = {}) => {
-  let middleware = [thunk];
-  if (process.env.NODE_ENV !== 'production') {
-    middleware = [...middleware, logger];
-  }
+  // let middleware = [thunk];
+  // if (process.env.NODE_ENV !== 'production') {
+  //   middleware = [...middleware, logger];
+  // }
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(...middleware)
+    applyMiddleware(thunk)
   );
 };
 
